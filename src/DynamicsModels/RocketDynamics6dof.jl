dbaf9bfce809e384e86b54c12d91a7023b1a1fc9
jakesonandres <jakeson@mit.edu>
2024-11-17 17:59:02 -0500

    Change dimension of control vector and propagate changes

--- src/DynamicsModels/RocketDynamics6dof.jl
+++ src/DynamicsModels/RocketDynamics6dof.jl
@@ -21,12 +21,18 @@ function dynamics6dof(x::AbstractVector, u::AbstractVector, params::Dict) :: Abs
     ωx, ωy, ωz = x[11:13]
     # Mass
     m = x[14]
-
+    # Position of engine wrt center of mass TODO: add to params
+    rTB = [0.0; 0.0; -14.0]
     # Extract control inputs
-    # Thrust force in body frame
-    Fbx, Fby, Fbz = u[1:3]
-    # Thrust moment in body frame
-    Mbx, Mby, Mbz = u[4:6]
+    TBx, TBy, TBz = u[1:3]
+    # Compute thrust and moment vectors
+    FIx, FIy, FIz = quaternion_to_rotation_matrix(q0, q1, q2, q3) * [TBx; TBy; TBz]
+    MBx, MBy, MBz = skew_symmetric3d(rTB) * [TBx; TBy; TBz]
+
+    # # Thrust force in body frame
+    # Fbx, Fby, Fbz = u[1:3]
+    # # Thrust moment in body frame
+    # Mbx, Mby, Mbz = u[4:6]
 
     #TODO: switch control vector to T
 
@@ -45,39 +51,41 @@ function dynamics6dof(x::AbstractVector, u::AbstractVector, params::Dict) :: Abs
 
     # Translational dynamics
     dr = [vx, vy, vz]
-    F_b = [Fbx, Fby, Fbz]
-    dv = (1 / m) * (R_b_to_i * F_b) + g_inertial
+    # F_b = [Fbx, Fby, Fbz]
+    # dv = (1 / m) * (R_b_to_i * F_b) + g_inertial
+    dv = (1 / m) * [FIx; FIy; FIz] + g_inertial 
 
     # Rotational dynamics
     q = [q0, q1, q2, q3]
     ω_b = [ωx, ωy, ωz]
-    dq = 0.5 * quaternion_product(q, [0.0; ω_b])
-
+    # dq = 0.5 * quaternion_product(q, [0.0; ω_b])
+    dq = 0.5 * skew_symmetric4d(ω_b) * q
     # Angular velocity derivative
-    M_b = [Mbx, Mby, Mbz]
-    dω = I_body \ (M_b - cross(ω_b, I_body * ω_b))
-
+    M_b = [MBx, MBy, MBz]
+    # dω = I_body \ (M_b - cross(ω_b, I_body * ω_b))
+    dω = I_body \ (M_b - skew_symmetric3d(ω_b) * I_body * ω_b)
     # Mass dynamics
-    F_b_magnitude = norm(F_b)
-    dm = -F_b_magnitude / (g0 * I_sp)
-
+    # F_b_magnitude = norm(F_b)
+    # dm = -F_b_magnitude / (g0 * I_sp)
+    mag_TB = norm([TBx; TBy; TBz])
+    dm = -mag_TB / (g0 * I_sp)
     # Concatenate derivatives
     dx = vcat(dr, dv, dq, dω, dm)
 
     return dx
 end
 
-# Quaternion to rotation matrix
+# Quaternion to rotation matrix #TODO: move to utils, rename to DCM
 function quaternion_to_rotation_matrix(q0::T, q1::T, q2::T, q3::T) :: AbstractMatrix{T} where T <: Real
     R = zeros(T, 3, 3)  # Generic matrix with type T
     R[1, 1] = 1 - 2 * (q2^2 + q3^2)
     R[1, 2] = 2 * (q1 * q2 - q3 * q0)
-    R[1, 3] = 2 * (q1 * q3 + q2 * q0)
-    R[2, 1] = 2 * (q1 * q2 + q3 * q0)
+    R[1, 3] = 2 * (q1 * q3 - q2 * q0)
+    R[2, 1] = 2 * (q1 * q2 - q3 * q0)
     R[2, 2] = 1 - 2 * (q1^2 + q3^2)
-    R[2, 3] = 2 * (q2 * q3 - q1 * q0)
-    R[3, 1] = 2 * (q1 * q3 - q2 * q0)
-    R[3, 2] = 2 * (q2 * q3 + q1 * q0)
+    R[2, 3] = 2 * (q2 * q3 + q1 * q0)
+    R[3, 1] = 2 * (q1 * q3 + q2 * q0)
+    R[3, 2] = 2 * (q2 * q3 - q1 * q0)
     R[3, 3] = 1 - 2 * (q1^2 + q2^2)
     return R
 end
@@ -125,5 +133,37 @@ function initialize_trajectory6dof(params::Dict) :: Tuple{AbstractArray{Real,2},
 
     return x_init, u_init
 end
-    
+
+# Helper function to compute skew-symmetric matrix of a vector TODO: move to utils
+function skew_symmetric3d(v::AbstractVector{T}) :: AbstractMatrix{T} where T <: Real
+    x, y, z = v
+    S = zeros(T, 3, 3)
+    S[1, 2] = -z
+    S[1, 3] = y
+    S[2, 1] = z
+    S[2, 3] = -x
+    S[3, 1] = -y
+    S[3, 2] = x
+    return S
+end
+
+function skew_symmetric4d(v::AbstractVector{T}) :: AbstractMatrix{T} where T <: Real
+    x, y, z = v
+    S = zeros(T, 4, 4)
+    S[1, 2] = -x
+    S[1, 3] = -y
+    S[1, 4] = -z
+    S[2, 1] = x
+    S[2, 3] = z
+    S[2, 4] = -y
+    S[3, 1] = y
+    S[3, 2] = -z
+    S[3, 4] = x
+    S[4, 1] = z
+    S[4, 2] = y
+    S[4, 3] = -x
+    return S
+end
+
+
 end # module RocketDynamics6dof
